9514cf329bca194c82d86510fc2d17aae3ef8ecb
taemin kim <97078686+ktm000818@users.noreply.github.com>
2022-09-27 02:25:39 +0900

    create MainPage component

---
+++ my-app/src/mainPage/MainPage.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import "./main_page.css"
+import { TextField } from "@material-ui/core";
+import Autocomplete from '@mui/material/Autocomplete';
+import UserSearchAutoComplete from "../Components/UserSearchAutoComplete";
+
+export default function MainPage() {
+
+    return (
+        <>
+            <div className='main_content'>
+                
+            </div>
+        </>
+    )
+}
