c00c6ef0988ee37606ab1f8716461073102cd390
kimtm0818 <kimtm0818@gmail.com>
2022-09-29 18:42:50 +0900

    debounce 메소드 개발

---
+++ my-app/src/Utils.tsx
@@ -0,0 +1,17 @@
+
+
+ let timer: NodeJS.Timeout;
+/**
+ * 
+ * @param callback 이벤트 완료 후 실행할 함수
+ */
+export function debounce(callback: () => void, timeout: number): void {
+    clearTimeout(timer);
+
+    setTimeout(() => {
+        if(typeof callback === 'function'){
+            callback();
+        }
+    }, timeout);
+
+}
