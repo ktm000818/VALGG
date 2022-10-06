dcbd895cb1b312ab6476e3cb9168482befc309dc
taemin kim <97078686+ktm000818@users.noreply.github.com>
2022-10-07 01:35:59 +0900

    update Profile.js : 데이터 연동 및 스타일 적용

--- my-app/src/profile/Profile.js
+++ my-app/src/profile/Profile.js
@@ -1,36 +1,109 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const react_1 = require("react");
-const react_router_dom_1 = require("react-router-dom");
-const RiotApi_1 = require("../store/RiotApi");
-require("./profile.css");
-function Profile() {
-    const [userData, setUserData] = (0, react_1.useState)({});
-    const location = (0, react_router_dom_1.useLocation)();
-    function fetchUserData() {
-        return __awaiter(this, void 0, void 0, function* () {
-            try {
-                const USER_DATA = yield (0, RiotApi_1.getAccountData)(location.state.username, location.state.tagline);
-                setUserData(USER_DATA);
-            }
-            catch (error) {
-                console.error(error);
-            }
-        });
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { getAccountDataTest, getAllUserData } from "../store/RiotApi";
+import './profile.css';
+
+export default function Profile() {
+    const [defaultUserData, setDefaultUserData] = useState({});
+    const [userData, setUserData] = useState({});
+    const { name, tag } = useLocation().state;
+
+    useEffect(() => {
+        getDefaultUserData()
+    }, [name])
+
+    useEffect(() => {
+        if (defaultUserData?.name)
+            setWholeUserData();
+    }, [defaultUserData])
+
+    /**
+     * 유저 기본정보 조회
+     */
+    async function getDefaultUserData() {
+        try {
+            const DEFAULT_USER_DATA = await getAccountDataTest({ name, tag }).then(res => res.data.data);
+            setDefaultUserData(DEFAULT_USER_DATA);
+
+        } catch (error) {
+            console.error(error);
+        }
     }
-    (0, react_1.useEffect)(() => {
-        fetchUserData();
-    }, [location.state.username]);
-    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)("div", Object.assign({ className: "profile_header_container" }, { children: (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "profile_header" }, { children: (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "profile_container" }, { children: (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "profile" }, { children: [(0, jsx_runtime_1.jsx)("div", Object.assign({ className: "profile_image_container" }, { children: (0, jsx_runtime_1.jsx)("img", { className: "profile_image" }) })), (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "profile_info_container" }, { children: [(0, jsx_runtime_1.jsxs)("div", { children: [(0, jsx_runtime_1.jsx)("strong", Object.assign({ className: "profile_name" }, { children: "\uB2C9\uB124\uC784" })), (0, jsx_runtime_1.jsx)("span", Object.assign({ className: "profile_tag" }, { children: "#TAG" }))] }), (0, jsx_runtime_1.jsxs)("div", { children: [(0, jsx_runtime_1.jsx)("span", Object.assign({ className: "profile_ladder" }, { children: "\uB798\uB354 \uB7AD\uD0B9" })), (0, jsx_runtime_1.jsx)("span", Object.assign({ className: "profile_ladder_rank" }, { children: "1231th" }))] }), (0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("button", Object.assign({ className: "history_update_button" }, { children: "\uC804\uC801 \uAC31\uC2E0" })) }), (0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("span", Object.assign({ className: "lastest_update_date" }, { children: "\uCD5C\uADFC \uC5C5\uB370\uC774\uD2B8: 2\uC77C \uC804" })) })] }))] })) })) })) })), (0, jsx_runtime_1.jsxs)("div", Object.assign({ style: { display: "flex", height: "100vh", margin: "1rem", padding: "1rem", border: "1px solid black" } }, { children: [(0, jsx_runtime_1.jsx)("div", { style: { flexGrow: "1", border: "1px solid black", marginRight: "1rem" } }), (0, jsx_runtime_1.jsx)("div", { style: { flexGrow: "2", border: "1px solid black", marginLeft: "1rem" } })] })), "name: ", userData.name, (0, jsx_runtime_1.jsx)("br", {}), "tag: ", userData.tag, (0, jsx_runtime_1.jsx)("br", {}), "level: ", userData.account_level, (0, jsx_runtime_1.jsx)("br", {}), "puuid: ", userData.puuid] }));
+
+    /**
+     * 유저의 전체 데이터 조회
+     */
+    async function getWholeUserData() {
+        const { name, tag, region, puuid } = defaultUserData;
+        const prop = {
+            name,
+            tag,
+            version: "v2",
+            region,
+            puuid
+        };
+
+        const result = await getAllUserData(prop).then(values => values);
+
+        const filteredResult = result.reduce((prev, curr) => {
+            // MMR History만 Array로 반환됨.
+            if (Array.isArray(curr.data.data)) {
+                return { ...prev, MMRHistory: curr.data.data }
+            } else {
+                return { ...Object.assign({ ...prev }, curr.data.data) }
+            }
+        }, {})
+
+        return filteredResult;
+    }
+
+    function setWholeUserData() {
+        getWholeUserData().then(res => {
+            setUserData(res)
+        })
+    }
+
+    useEffect(() => {
+        console.log(userData)
+    }, [userData])
+
+    return (
+        <>
+            <div className="profile_header_container">
+                <div className="profile_header">
+                    <div className="profile_container">
+                        <div className="profile">
+                            <div className="profile_image_container">
+                                <img className="profile_image" src={userData?.card?.small} />
+                            </div>
+                            <div className="profile_info_container">
+                                <div>
+                                    <strong className="profile_name">{defaultUserData.name}</strong>
+                                    <span className="profile_tag">#{defaultUserData.tag}</span>
+                                </div>
+                                <div>
+                                    <span className="profile_ladder">래더 랭킹</span>
+                                    <span className="profile_ladder_rank">1231th</span>
+                                </div>
+                                <div>
+                                    <button className="history_update_button" onClick={setWholeUserData}>전적 갱신</button>
+                                </div>
+                                <div>
+                                    <span className="lastest_update_date">최근 업데이트: {userData.last_update}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style={{ display: "flex", height: "100vh", margin: "1rem", padding: "1rem", border: "1px solid black" }}>
+                <div style={{ flexGrow: "1", border: "1px solid black", marginRight: "1rem" }}>
+
+                </div>
+                <div style={{ flexGrow: "2", border: "1px solid black", marginLeft: "1rem" }}>
+
+                </div>
+            </div>
+        </>
+    )
 }
-exports.default = Profile;
