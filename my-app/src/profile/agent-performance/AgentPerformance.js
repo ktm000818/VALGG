494a01882ea7e167fdf7ea52c52d97a106e031d7
kimtm0818 <kimtm0818@gmail.com>
2022-10-13 22:57:14 +0900

    update AgentPerformance.js : ui 구현

--- my-app/src/profile/agent-performance/AgentPerformance.js
+++ my-app/src/profile/agent-performance/AgentPerformance.js
@@ -1,7 +1,18 @@
+import React, { userState } from 'react';
 import './agent_performance.css';
 
-export default function AgentPerfomance() {
+export default function AgentPerfomance({ userData }) {
 
+    const [showMore, setShowMore] = useState(false);
+
+    function toggleShowMoreAgentInfo() {
+        setShowMore(true);
+    }
+
+    const infos = [
+        { agentName: "zett", avgScore: 340, avgRecord: 1.99, kills: 152, deaths: 91, assists: 29, playedMatch: 3, winRatio: 80 },
+        { agentName: "Omen", avgScore: 400, avgRecord: 2.49, kills: 999, deaths: 91, assists: 29, playedMatch: 2, winRatio: 100 },
+    ]
 
     return (
         <>
@@ -16,7 +27,7 @@ export default function AgentPerfomance() {
                     <div className="agent_performance_info_container">
                         <div className="agent_performance_info">
                             <div className="agent_performance_info_agent_image_container">
-                                <img width={20} height={20}/>
+                                <img width={20} height={20} />
                             </div>
                             <div className="agent_performance_info_agent">
                                 <span className="agent_performance_agent">제트</span>
@@ -37,9 +48,43 @@ export default function AgentPerfomance() {
                         <div className="agent_performance_info">
                             dfdf
                         </div>
+                        {infos.map((info, index) => {
+
+                            if (!showMore && index > 2) {
+                                return null;
+                            }
+
+                            return (
+                                <>
+                                    <div className="agent_performance_info">
+                                        <div className="agent_performance_info_agent_image_container">
+                                            <img width={20} height={20} />
+                                        </div>
+                                        <div className="agent_performance_info_agent">
+                                            <span className="agent_performance_agent">{info.agentName}</span>
+                                            <span className="agent_performance_avgscore">{info.avgScore} 평균 점수</span>
+                                        </div>
+                                        <div className="agent_performance_info_record">
+                                            <span className="agent_performance_record">{info.avgRecord}:1 평점</span>
+                                            <span className="agent_performance_kda">`${info.kills}/${info.deaths}/${info.assists}`</span>
+                                        </div>
+                                        <div className="agent_performance_info_winratio">
+                                            <span className="agent_performance_winratio">{info.winRatio}%</span>
+                                            <span className="agent_performance_matchcount">{info.playedMatch}게임</span>
+                                        </div>
+                                    </div>
+                                </>
+                            )
+                        })}
+                        <div className="agent_performance_info">
+                            <span style={{textAlign: "center"}}>
+                                {showMore ? "닫기" : "더보기"}
+                            </span>
+                        </div>
+
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+            )
 }
