3884d5d6af8ba4e2423f92f1e1e9490c72b0f24a
taemin kim <97078686+ktm000818@users.noreply.github.com>
2023-02-11 17:38:14 +0900

    refactor: apply emotion at Header.js

--- my-app/src/header/Header.js
+++ my-app/src/header/Header.js
@@ -1,9 +1,11 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CustomAutoComplete from "../components/CustomAutoComplete";
 import { getAccountData } from "../store/RiotApi";
 import { debounce } from "../Utils";
-import "./header.css";
+import {ReactComponent as TitleImage} from '../assets/images/valorant.svg'
 
 export default function Header() {
 
@@ -11,7 +13,7 @@ export default function Header() {
     const navigate = useNavigate();
 
     const handleChangeAutoComplete = (value) => {
-        if (value) {
+        if (value.includes("#")) {
             const [name, tag] = value.split("#");
             navigate(`/profile?name=${name}&tag=${tag}`, {
                 state: {
@@ -22,7 +24,7 @@ export default function Header() {
         }
     };
 
-    const handleChangeTextField = async (value) => {
+    const handleChangeInput = async (value) => {
         if (value.includes("#")) {
             debounce(async () => {
                 const [name, tag] = value.split("#");
@@ -35,30 +37,37 @@ export default function Header() {
     };
 
     return (
-        <div className="header">
-            <div>
-                <svg
-                    fill="#000000"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 50 50"
-                    width="70px"
-                    height="70px"
-                >
-                    <path d="M4.781 6.375C4.515 6.044 4.067 5.916 3.669 6.057 3.268 6.197 3 6.575 3 7v18c0 
-                        .232.081.457.228.636l14 17C17.418 42.866 17.701 43 18 43h14c.384 0 
-                        .735-.221.901-.566.167-.347.12-.758-.121-1.059L4.781 6.375zM46.336 
-                        7.059c-.396-.146-.842-.02-1.11.309l-18 22c-.245.299-.295.712-.13 
-                        1.062C27.262 30.777 27.614 31 28 31h14c.304 0 .591-.138.781-.375l4-5C46.923 
-                        25.447 47 25.228 47 25V8C47 7.577 46.734 7.2 46.336 7.059z"
-                    />
-                </svg>
-
-                <div style={{ display: "inline-block", alignItems: "center" }}>
-                    VALORANT
-                </div>
-            </div>
-            <CustomAutoComplete options={userList} onInputChange={handleChangeTextField} onChange={handleChangeAutoComplete} />
-        </div>
+        <HeaderWrapper>
+            <TitleImageWrapper>
+                <TitleImage/>
+                <HeaderTitle>Valorant</HeaderTitle>
+            </TitleImageWrapper>
+            <CustomAutoComplete options={userList} onInputChange={handleChangeInput} onChange={handleChangeAutoComplete} />
+        </HeaderWrapper>
     )
 }
 
+const HeaderWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #E84057;
+    height: 80px;
+`
+
+const TitleImageWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+`
+
+const HeaderTitle = styled.div`
+    height: 70px;
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    font-size: 2rem;
+    font-weight: bold;
+`
+
+// css
