f93952be0321c6598ab9069d91351e6406756eeb
taemin kim <97078686+ktm000818@users.noreply.github.com>
2023-02-26 20:28:07 +0900

    feat: apply TypeScript at ProfileCard

--- my-app/src/profile/profile-card/ProfileCard.js
+++ my-app/src/profile/profile-card/ProfileCard.js
@@ -1,111 +1,86 @@
-import styled from "@emotion/styled";
-import { useRecoilValue } from "recoil"
-import { playerDefaultInfoState } from "../../store/playerWholeInfoStore"
-
-export default function ProfileCard({ updatePlayerInfo = () => { } }) {
-    const playerDefaultInfo = useRecoilValue(playerDefaultInfoState);
-    return (
-        <>
-            <ProfileCardWrapper>
-                <ProfileCardContent>
-                    <ProfileWrapper>
-                        <Profile>
-                            <ProfileImageWrapper>
-                                <ProfileImage src={playerDefaultInfo?.card?.small} />
-                            </ProfileImageWrapper>
-                            <ProfileInfoWrapper>
-                                <ProfileInfo>
-                                    <ProfileName>{playerDefaultInfo?.name}</ProfileName>
-                                    <ProfileTag>#{playerDefaultInfo?.tag}</ProfileTag>
-                                </ProfileInfo>
-                                <ProfileInfo>
-                                    <ProfileRankLabel>래더 랭킹</ProfileRankLabel>
-                                    <ProfileLadderRank>1231th</ProfileLadderRank>
-                                </ProfileInfo>
-                                <ProfileInfo>
-                                    <HistoryUpdateButton onClick={updatePlayerInfo}>전적 갱신</HistoryUpdateButton>
-                                </ProfileInfo>
-                                <ProfileInfo>
-                                    <LatestUpdateDate>최근 업데이트: {playerDefaultInfo?.last_update}</LatestUpdateDate>
-                                </ProfileInfo>
-                            </ProfileInfoWrapper>
-                        </Profile>
-                    </ProfileWrapper>
-                </ProfileCardContent>
-            </ProfileCardWrapper>
-        </>
-    )
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+const styled_1 = __importDefault(require("@emotion/styled"));
+const material_1 = require("@mui/material");
+const lodash_1 = require("lodash");
+const react_1 = require("react");
+const recoil_1 = require("recoil");
+const playerWholeInfoStore_1 = require("../../store/playerWholeInfoStore");
+function ProfileCard({ updatePlayerInfo = () => { } }) {
+    const playerDefaultInfo = (0, recoil_1.useRecoilValue)(playerWholeInfoStore_1.playerDefaultInfoState);
+    const [loading, setLoading] = (0, react_1.useState)(false);
+    (0, react_1.useEffect)(() => {
+        if ((0, lodash_1.isEmpty)(playerDefaultInfo)) {
+            setLoading(true);
+        }
+        else {
+            setLoading(false);
+        }
+    }, [playerDefaultInfo]);
+    return ((0, jsx_runtime_1.jsx)(jsx_runtime_1.Fragment, { children: (0, jsx_runtime_1.jsx)(ProfileCardWrapper, { children: (0, jsx_runtime_1.jsx)(ProfileCardContent, { children: (0, jsx_runtime_1.jsx)(ProfileWrapper, { children: (0, jsx_runtime_1.jsxs)(Profile, { children: [(0, jsx_runtime_1.jsx)(ProfileImageWrapper, { children: (0, jsx_runtime_1.jsx)(jsx_runtime_1.Fragment, { children: loading ? ((0, jsx_runtime_1.jsx)(material_1.Skeleton, Object.assign({ variant: "rectangular" }, { children: (0, jsx_runtime_1.jsx)(ProfileImage, {}) }))) : ((0, jsx_runtime_1.jsx)(ProfileImage, { src: (0, lodash_1.get)(playerDefaultInfo, 'card.small', '') })) }) }), (0, jsx_runtime_1.jsxs)(ProfileInfoWrapper, { children: [(0, jsx_runtime_1.jsx)(ProfileInfo, { children: loading ? ((0, jsx_runtime_1.jsx)(material_1.Skeleton, { variant: "text", sx: { fontSize: "24px" } })) : ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)(ProfileName, { children: (0, lodash_1.get)(playerDefaultInfo, 'name', '') }), (0, jsx_runtime_1.jsx)(ProfileTag, { children: `#${(0, lodash_1.get)(playerDefaultInfo, 'tag', '')}` })] })) }), (0, jsx_runtime_1.jsxs)(ProfileInfo, { children: [(0, jsx_runtime_1.jsx)(ProfileRankLabel, { children: "\uB798\uB354 \uB7AD\uD0B9" }), (0, jsx_runtime_1.jsx)(ProfileLadderRank, { children: "1231th" })] }), (0, jsx_runtime_1.jsx)(ProfileInfo, { children: (0, jsx_runtime_1.jsx)(HistoryUpdateButton, Object.assign({ onClick: updatePlayerInfo }, { children: "\uC804\uC801 \uAC31\uC2E0" })) }), (0, jsx_runtime_1.jsx)(ProfileInfo, { children: loading ? ((0, jsx_runtime_1.jsx)(material_1.Skeleton, { variant: "text", sx: { fontSize: "11px" } })) : ((0, jsx_runtime_1.jsxs)(LatestUpdateDate, { children: ["\uCD5C\uADFC \uC5C5\uB370\uC774\uD2B8: ", (0, lodash_1.get)(playerDefaultInfo, 'last_update', '')] })) })] })] }) }) }) }) }));
 }
-
-const ProfileCardWrapper = styled.div`
+exports.default = ProfileCard;
+const ProfileCardWrapper = styled_1.default.div `
     height: 200px;
     width: 100%;
     background: rgb(49, 49, 60);
-`
-
-const ProfileCardContent = styled.div`
+`;
+const ProfileCardContent = styled_1.default.div `
     width: 1300px;
     padding: 0 110px;
     margin: 0 auto;
     box-sizing: border-box;
-`
-
-const ProfileWrapper = styled.div`
+`;
+const ProfileWrapper = styled_1.default.div `
     height: 200px;
     display: flex;
     padding: 46px 0 24px;
     box-sizing: border-box;
     background: url("https://opgg-valorant-cdn.akamaized.net/Characters/TopBars/Omen.png") no-repeat center top / contain ;
-`
-
-const Profile = styled.div`
+`;
+const Profile = styled_1.default.div `
     display: flex;
     align-items: center;
-`
-
-const ProfileImageWrapper = styled.div`
+`;
+const ProfileImageWrapper = styled_1.default.div `
     display: flex;
     border-radius: 20px;
     overflow: hidden;
-`
-
-const ProfileImage = styled.img`
+`;
+const ProfileImage = styled_1.default.img `
     width: 100px;
     height: 100px;
-`
-
-const ProfileInfoWrapper = styled.div`
+`;
+const ProfileInfoWrapper = styled_1.default.div `
     padding: 0 24px;
-`
-
-const ProfileInfo = styled.div`
-`
-
-const ProfileName = styled.strong`
+`;
+const ProfileInfo = styled_1.default.div `
+`;
+const ProfileName = styled_1.default.span `
     font-size: 24px;
-`
-
-const ProfileTag = styled.span`
+    font-weight: bold;
+`;
+const ProfileTag = styled_1.default.span `
     color: gray;
-`
-
-const ProfileRankLabel = styled.span`
+`;
+const ProfileRankLabel = styled_1.default.span `
     color: gray;
     font-size: 11px;
-`
-
-const ProfileLadderRank = styled.span`
+`;
+const ProfileLadderRank = styled_1.default.span `
     color: lightblue;
     font-size: 11px;
     margin-left: 4px;
-`
-
-const LatestUpdateDate = styled.span`
+`;
+const LatestUpdateDate = styled_1.default.span `
     color: white;
     font-size: 11px;
-`
-
-const HistoryUpdateButton = styled.button`
+`;
+const HistoryUpdateButton = styled_1.default.button `
     height: 40px;
     padding: 0 16px;
     border-radius: 5px;
@@ -118,4 +93,4 @@ const HistoryUpdateButton = styled.button`
     &:hover {
         background-color: #AC2537;
     }
-`
+`;
---
+++ my-app/src/profile/profile-card/ProfileCard.tsx
@@ -0,0 +1,154 @@
+import styled from "@emotion/styled";
+import { Skeleton } from "@mui/material";
+import { get, isEmpty } from "lodash";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil"
+import { playerDefaultInfoState } from "../../store/playerWholeInfoStore"
+import { DEFAULT_USER_DATA } from "../../store/RiotApi";
+
+export default function ProfileCard({ updatePlayerInfo = () => { } }) {
+    const playerDefaultInfo: DEFAULT_USER_DATA | unknown = useRecoilValue(playerDefaultInfoState);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (isEmpty(playerDefaultInfo)) {
+            setLoading(true);
+        } else {
+            setLoading(false);
+        }
+    }, [playerDefaultInfo])
+
+    return (
+        <>
+            <ProfileCardWrapper>
+                <ProfileCardContent>
+                    <ProfileWrapper>
+                        <Profile>
+                            <ProfileImageWrapper>
+                                <>
+                                    {loading ? (
+                                        <Skeleton variant="rectangular">
+                                            <ProfileImage />
+                                        </Skeleton>
+                                    ) : (
+                                        <ProfileImage src={get(playerDefaultInfo, 'card.small', '')} />
+                                    )}
+                                </>
+                            </ProfileImageWrapper>
+                            <ProfileInfoWrapper>
+                                <ProfileInfo>
+                                    {loading ? (
+                                        <Skeleton variant="text" sx={{ fontSize: "24px" }} />
+                                    ) : (
+                                        <>
+                                            <ProfileName>{get(playerDefaultInfo, 'name', '')}</ProfileName>
+                                            <ProfileTag>{`#${get(playerDefaultInfo, 'tag', '')}`}</ProfileTag>
+                                        </>
+                                    )}
+                                </ProfileInfo>
+                                <ProfileInfo>
+                                    <ProfileRankLabel>래더 랭킹</ProfileRankLabel>
+                                    <ProfileLadderRank>1231th</ProfileLadderRank>
+                                </ProfileInfo>
+                                <ProfileInfo>
+                                    <HistoryUpdateButton onClick={updatePlayerInfo}>전적 갱신</HistoryUpdateButton>
+                                </ProfileInfo>
+                                <ProfileInfo>
+                                    {loading ? (
+                                        <Skeleton variant="text" sx={{ fontSize: "11px" }} />
+                                    ) : (
+                                        <LatestUpdateDate>최근 업데이트: {get(playerDefaultInfo, 'last_update', '')}</LatestUpdateDate>
+                                    )}
+                                </ProfileInfo>
+                            </ProfileInfoWrapper>
+                        </Profile>
+                    </ProfileWrapper>
+                </ProfileCardContent>
+            </ProfileCardWrapper>
+        </>
+    )
+}
+
+const ProfileCardWrapper = styled.div`
+    height: 200px;
+    width: 100%;
+    background: rgb(49, 49, 60);
+`
+
+const ProfileCardContent = styled.div`
+    width: 1300px;
+    padding: 0 110px;
+    margin: 0 auto;
+    box-sizing: border-box;
+`
+
+const ProfileWrapper = styled.div`
+    height: 200px;
+    display: flex;
+    padding: 46px 0 24px;
+    box-sizing: border-box;
+    background: url("https://opgg-valorant-cdn.akamaized.net/Characters/TopBars/Omen.png") no-repeat center top / contain ;
+`
+
+const Profile = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const ProfileImageWrapper = styled.div`
+    display: flex;
+    border-radius: 20px;
+    overflow: hidden;
+`
+
+const ProfileImage = styled.img`
+    width: 100px;
+    height: 100px;
+`
+
+const ProfileInfoWrapper = styled.div`
+    padding: 0 24px;
+`
+
+const ProfileInfo = styled.div`
+`
+
+const ProfileName = styled.span`
+    font-size: 24px;
+    font-weight: bold;
+`
+
+const ProfileTag = styled.span`
+    color: gray;
+`
+
+const ProfileRankLabel = styled.span`
+    color: gray;
+    font-size: 11px;
+`
+
+const ProfileLadderRank = styled.span`
+    color: lightblue;
+    font-size: 11px;
+    margin-left: 4px;
+`
+
+const LatestUpdateDate = styled.span`
+    color: white;
+    font-size: 11px;
+`
+
+const HistoryUpdateButton = styled.button`
+    height: 40px;
+    padding: 0 16px;
+    border-radius: 5px;
+    color: lightgray;
+    background-color: #E84057;
+    font-weight: bold;
+    margin-top: 9px;
+    border: 0px;
+    cursor: pointer;
+    &:hover {
+        background-color: #AC2537;
+    }
+`
