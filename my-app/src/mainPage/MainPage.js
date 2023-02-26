b950ed02e160ba4282b3f2eb4d872ab20969b605
taemin kim <97078686+ktm000818@users.noreply.github.com>
2023-02-26 20:27:31 +0900

    feat: apply TypeScript at MainPage

--- my-app/src/mainPage/MainPage.js
+++ my-app/src/mainPage/MainPage.js
@@ -1,87 +1,82 @@
-import styled from '@emotion/styled';
-import { isEmpty, uniqBy } from 'lodash';
-import { useCallback, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import CustomAutoComplete from '../components/CustomAutoComplete';
-import { getAccountData } from '../store/RiotApi';
-import { debounce } from '../Utils';
-
-export default function MainPage() {
-
-    const LS_SEARCH_HISTORY = JSON.parse(localStorage.getItem("searchHistory")) ?? [];
-    const [userList, setUserList] = useState({name: ''});
-    const navigate = useNavigate();
-
-    const handleChangeAutoComplete = useCallback((value) => {
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+const styled_1 = __importDefault(require("@emotion/styled"));
+const lodash_1 = require("lodash");
+const react_1 = require("react");
+const react_router_dom_1 = require("react-router-dom");
+const CustomAutoComplete_1 = __importDefault(require("../components/CustomAutoComplete"));
+const RiotApi_1 = require("../store/RiotApi");
+const Utils_1 = require("../Utils");
+function MainPage() {
+    var _a;
+    const LS_SEARCH_HISTORY = JSON.parse((_a = localStorage.getItem("searchHistory")) !== null && _a !== void 0 ? _a : "[]");
+    const [userList, setUserList] = (0, react_1.useState)([{ name: "" }]);
+    const navigate = (0, react_router_dom_1.useNavigate)();
+    const handleChangeAutoComplete = (0, react_1.useCallback)((value) => {
+        var _a;
         if (value) {
             const [name, tag] = value.split("#");
-
-            let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) ?? [];
-            searchHistory.push({name: value});
-            localStorage.setItem("searchHistory", JSON.stringify(Array.from(uniqBy(searchHistory, "name"))));
-
+            let searchHistory = JSON.parse((_a = localStorage.getItem("searchHistory")) !== null && _a !== void 0 ? _a : "[]");
+            searchHistory.push({ name: value });
+            localStorage.setItem("searchHistory", JSON.stringify(Array.from((0, lodash_1.uniqBy)(searchHistory, "name"))));
             navigate(`/profile?name=${name}&tag=${tag}`, {
                 state: {
                     name,
                     tag
                 }
-            })
+            });
         }
-    }, [navigate])
-
-    const handleChangeTextField = useCallback(async (value) => {
+    }, [navigate]);
+    const handleChangeTextField = (0, react_1.useCallback)((value) => __awaiter(this, void 0, void 0, function* () {
         if (value.includes("#")) {
-            debounce(async () => {
+            (0, Utils_1.debounce)(() => __awaiter(this, void 0, void 0, function* () {
                 const [name, tag] = value.split("#");
-                const USER_LIST = await getAccountData(name, tag, true);
+                const USER_LIST = yield (0, RiotApi_1.getAccountData)(name, tag, true);
                 setUserList([USER_LIST]);
-            }, 500)
-        } else {
+            }), 500);
+        }
+        else {
             setUserList([{ name: '' }]);
         }
-    }, [userList])
-
-    return (
-        <>
-            <MainWrapper>
-                <MainContentWrapper>
-                    <MainContentLogoImageWrapper>
-                        <img src={"https://valorant.op.gg/images/valorant.png"} width="240px" />
-                    </MainContentLogoImageWrapper>
-                    <AutoCompleteWrapper>
-                        <CustomAutoComplete searchHistory={LS_SEARCH_HISTORY} options={userList} onInputChange={handleChangeTextField} onChange={handleChangeAutoComplete} style={{ width: "500px" }} />
-                    </AutoCompleteWrapper>
-                </MainContentWrapper>
-            </MainWrapper>
-        </>
-    )
+    }), [userList]);
+    return ((0, jsx_runtime_1.jsx)(jsx_runtime_1.Fragment, { children: (0, jsx_runtime_1.jsx)(MainWrapper, { children: (0, jsx_runtime_1.jsxs)(MainContentWrapper, { children: [(0, jsx_runtime_1.jsx)(MainContentLogoImageWrapper, { children: (0, jsx_runtime_1.jsx)("img", { src: "https://valorant.op.gg/images/valorant.png", width: "240px" }) }), (0, jsx_runtime_1.jsx)(AutoCompleteWrapper, { children: (0, jsx_runtime_1.jsx)(CustomAutoComplete_1.default, { id: "main_auto_complete", searchHistory: LS_SEARCH_HISTORY, options: userList, onInputChange: handleChangeTextField, onChange: handleChangeAutoComplete, style: { width: "500px" } }) })] }) }) }));
 }
-
-const MainWrapper = styled.div`
+exports.default = MainPage;
+const MainWrapper = styled_1.default.div `
     background: url(https://valorant.op.gg/images/main_bg_desktop.jpeg) no-repeat center top;
     position: relative;
     min-height: 1080px;
     background-size: cover;
-`
-
-const MainContentWrapper = styled.div`
+`;
+const MainContentWrapper = styled_1.default.div `
     position: relative;
     width: 50%;
     height: 900px;
     margin: auto;
     padding-top: 50px;
-`
-
-const MainContentLogoImageWrapper = styled.div`
+`;
+const MainContentLogoImageWrapper = styled_1.default.div `
     width: 100%;
     position: absolute;
     left: 0;
-`
-
-const AutoCompleteWrapper = styled.div`
+`;
+const AutoCompleteWrapper = styled_1.default.div `
     position: absolute;
     margin: auto;
     top: 40%;
     left: 50%;
     transform: translate(-50%, -50%);
-`
+`;
