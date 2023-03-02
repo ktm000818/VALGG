683f971605c1299ad6e08176045483d734acba99
taemin kim <97078686+ktm000818@users.noreply.github.com>
2023-03-02 23:47:16 +0900

    chore: edit index.js and App.test.js

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
+const jsx_runtime_1 = require("@emotion/react/jsx-runtime");
 const react_1 = require("@testing-library/react");
 const App_1 = __importDefault(require("./App"));
 test('renders learn react link', () => {
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
+const jsx_runtime_1 = require("@emotion/react/jsx-runtime");
 const client_1 = __importDefault(require("react-dom/client"));
 require("./index.css");
 const App_1 = __importDefault(require("./App"));
