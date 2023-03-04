67d915ac9facab08d0415760b55dc288aca9aeee
taemin kim <97078686+ktm000818@users.noreply.github.com>
2023-03-05 00:18:57 +0900

    feat: add ErrorBoundary at App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,20 @@ import "./App.css";
 import Profile from "./profile/Profile";
 import Header from "./header/Header";
 import { RecoilRoot } from "recoil";
+import ErrorBoundary from "./ErrorBoundary";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <RecoilRoot>
-          <Header />
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
+          <ErrorBoundary>
+            <Header />
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </ErrorBoundary>
         </RecoilRoot>
       </BrowserRouter>
     </>
