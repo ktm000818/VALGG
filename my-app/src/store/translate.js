750a4e04c0a5ccf7906c8654ff09ad769ea3ccb0
taemin kim <97078686+ktm000818@users.noreply.github.com>
2023-03-05 00:20:28 +0900

    feat: add tier translate func

--- my-app/src/store/translate.js
+++ my-app/src/store/translate.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAgentRole = exports.getAgentName = exports.agent = void 0;
+exports.getTierName = exports.getAgentRole = exports.getAgentName = exports.tier = exports.agent = void 0;
 class Agent {
     constructor(name_us, name_kr, role_us, role_kr) {
         this.localization = {
@@ -15,8 +15,20 @@ class Agent {
         };
     }
 }
+class Tier {
+    constructor(name_us, name_kr) {
+        this.localization = {
+            en_us: {
+                name: name_us
+            },
+            ko_kr: {
+                name: name_kr
+            }
+        };
+    }
+}
 exports.agent = {
-    Zett: new Agent("Zett", "제트", "Duelist", "타격대"),
+    Jett: new Agent("Jett", "제트", "Duelist", "타격대"),
     Raze: new Agent("Raze", "레이즈", "Duelist", "타격대"),
     Breach: new Agent("Breach", "브리치", "Initiator", "척후대"),
     Omen: new Agent("Omen", "오멘", "Controller", "전략가"),
@@ -37,13 +49,48 @@ exports.agent = {
     Fade: new Agent("Fade", "페이드", "Initiator", "척후대"),
     Harbor: new Agent("Harbor", "하버", "Controller", "전략가"),
 };
+exports.tier = {
+    'Iron 1': new Tier("Iron 1", "아이언 1"),
+    'Iron 2': new Tier("Iron 2", "아이언 2"),
+    'Iron 3': new Tier("Iron 3", "아이언 3"),
+    'Bronze 1': new Tier("Bronze 1", "브론즈 1"),
+    'Bronze 2': new Tier("Bronze 2", "브론즈 2"),
+    'Bronze 3': new Tier("Bronze 3", "브론즈 3"),
+    'Silver 1': new Tier("Silver 1", "실버 1"),
+    'Silver 2': new Tier("Silver 2", "실버 2"),
+    'Silver 3': new Tier("Silver 3", "실버 3"),
+    'Gold 1': new Tier("Gold 1", "골드 1"),
+    'Gold 2': new Tier("Gold 2", "골드 2"),
+    'Gold 3': new Tier("Gold 3", "골드 3"),
+    'Platinum 1': new Tier("Platinum 1", "플래티넘 1"),
+    'Platinum 2': new Tier("Platinum 2", "플래티넘 2"),
+    'Platinum 3': new Tier("Platinum 3", "플래티넘 3"),
+    'Diamond 1': new Tier("Diamond 1", "다이아몬드 1"),
+    'Diamond 2': new Tier("Diamond 2", "다이아몬드 2"),
+    'Diamond 3': new Tier("Diamond 3", "다이아몬드 3"),
+    'Ascentdant 1': new Tier("Ascentdant 1", "초월자 1"),
+    'Ascentdant 2': new Tier("Ascentdant 2", "초월자 2"),
+    'Ascentdant 3': new Tier("Ascentdant 3", "초월자 3"),
+    'Immortal 1': new Tier("Immortal 1", "불멸 1"),
+    'Immortal 2': new Tier("Immortal 2", "불멸 2"),
+    'Immortal 3': new Tier("Immortal 3", "불멸 3"),
+    'Radiant': new Tier("Radiant", "레디언트"),
+};
 const getAgentName = (name) => {
+    var _a, _b;
     const legion = "ko_kr"; // 국가 설정 필요
-    return exports.agent[name]['localization'][legion].name;
+    return (_b = (_a = exports.agent[name]) === null || _a === void 0 ? void 0 : _a['localization'][legion].name) !== null && _b !== void 0 ? _b : name;
 };
 exports.getAgentName = getAgentName;
 const getAgentRole = (name) => {
+    var _a, _b, _c;
     const legion = "ko_kr"; // 국가 설정 필요
-    return exports.agent[name]['localization'][legion].role;
+    return (_c = (_b = (_a = exports.agent[name]) === null || _a === void 0 ? void 0 : _a['localization'][legion]) === null || _b === void 0 ? void 0 : _b.role) !== null && _c !== void 0 ? _c : '';
 };
 exports.getAgentRole = getAgentRole;
+const getTierName = (name) => {
+    var _a, _b;
+    const legion = "ko_kr"; // 국가 설정 필요
+    return (_b = (_a = exports.tier[name]) === null || _a === void 0 ? void 0 : _a['localization'][legion].name) !== null && _b !== void 0 ? _b : name;
+};
+exports.getTierName = getTierName;
--- my-app/src/store/translate.ts
+++ my-app/src/store/translate.ts
@@ -1,22 +1,36 @@
-interface En_us {
+interface Agent_en_us {
     name: string
     role: string
 }
 
-interface Ko_kr {
+interface Agent_Ko_kr {
     name: string
     role: string
+}
+
+interface Tier_en_us {
+    name: string
+}
+
+interface Tier_Ko_kr {
+    name: string
+}
+
+
+interface LocalizationAgent {
+    en_us: Agent_en_us
+    ko_kr: Agent_Ko_kr
 }
 
 interface Localization {
-    en_us: En_us
-    ko_kr: Ko_kr
+    en_us: Tier_en_us
+    ko_kr: Tier_Ko_kr
 }
 
 
 
 class Agent {
-    localization: Localization
+    localization: LocalizationAgent
 
     constructor(name_us: string, name_kr: string, role_us: string, role_kr: string) {
         this.localization = {
@@ -32,8 +46,24 @@ class Agent {
     }
 }
 
+class Tier {
+    localization: Localization
+
+    constructor(name_us: string, name_kr: string) {
+        this.localization = {
+            en_us: {
+                name: name_us
+            },
+            ko_kr: {
+                name: name_kr
+            }
+        }
+    }
+}
+
+
 export const agent = {
-    Zett: new Agent("Zett", "제트", "Duelist", "타격대"),
+    Jett: new Agent("Jett", "제트", "Duelist", "타격대"),
     Raze: new Agent("Raze", "레이즈", "Duelist", "타격대"),
     Breach: new Agent("Breach", "브리치", "Initiator", "척후대"),
     Omen: new Agent("Omen", "오멘", "Controller", "전략가"),
@@ -55,14 +85,48 @@ export const agent = {
     Harbor: new Agent("Harbor", "하버", "Controller", "전략가"),
 } as const
 
+export const tier = {
+    'Iron 1': new Tier("Iron 1", "아이언 1"),
+    'Iron 2': new Tier("Iron 2", "아이언 2"),
+    'Iron 3': new Tier("Iron 3", "아이언 3"),
+    'Bronze 1': new Tier("Bronze 1", "브론즈 1"),
+    'Bronze 2': new Tier("Bronze 2", "브론즈 2"),
+    'Bronze 3': new Tier("Bronze 3", "브론즈 3"),
+    'Silver 1': new Tier("Silver 1", "실버 1"),
+    'Silver 2': new Tier("Silver 2", "실버 2"),
+    'Silver 3': new Tier("Silver 3", "실버 3"),
+    'Gold 1': new Tier("Gold 1", "골드 1"),
+    'Gold 2': new Tier("Gold 2", "골드 2"),
+    'Gold 3': new Tier("Gold 3", "골드 3"),
+    'Platinum 1': new Tier("Platinum 1", "플래티넘 1"),
+    'Platinum 2': new Tier("Platinum 2", "플래티넘 2"),
+    'Platinum 3': new Tier("Platinum 3", "플래티넘 3"),
+    'Diamond 1': new Tier("Diamond 1", "다이아몬드 1"),
+    'Diamond 2': new Tier("Diamond 2", "다이아몬드 2"),
+    'Diamond 3': new Tier("Diamond 3", "다이아몬드 3"),
+    'Ascentdant 1': new Tier("Ascentdant 1", "초월자 1"),
+    'Ascentdant 2': new Tier("Ascentdant 2", "초월자 2"),
+    'Ascentdant 3': new Tier("Ascentdant 3", "초월자 3"),
+    'Immortal 1': new Tier("Immortal 1", "불멸 1"),
+    'Immortal 2': new Tier("Immortal 2", "불멸 2"),
+    'Immortal 3': new Tier("Immortal 3", "불멸 3"),
+    'Radiant': new Tier("Radiant", "레디언트"),
+} as const
+
 export const getAgentName: (name: string) => string = (name) => {
     const legion = "ko_kr"; // 국가 설정 필요
 
-    return agent[name as keyof typeof agent]['localization'][legion].name;
+    return agent[name as keyof typeof agent]?.['localization'][legion].name ?? name;
 }
 
 export const getAgentRole: (name: string) => string = (name) => {
     const legion = "ko_kr"; // 국가 설정 필요
 
-    return agent[name as keyof typeof agent]['localization'][legion].role;
+    return agent[name as keyof typeof agent]?.['localization'][legion]?.role ?? '';
+}
+
+export const getTierName: (name: string) => string = (name) => {
+    const legion = "ko_kr"; // 국가 설정 필요
+
+    return tier[name as keyof typeof tier]?.['localization'][legion].name ?? name;
 }
