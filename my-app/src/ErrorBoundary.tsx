cd098981ee671a71e13b2ba3e8c0ab4d8595565c
taemin kim <97078686+ktm000818@users.noreply.github.com>
2023-03-11 01:45:55 +0900

    feat: apply tsx at ErrorBoundary.js

---
+++ my-app/src/ErrorBoundary.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface State {
+  hasError: boolean
+}
+
+interface Props {
+  children: React.ReactNode
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+    state: State = {
+      hasError: false
+    }
+  
+    //prop error 
+    static getDerivedStateFromError(error: any) {
+      // Update state so the next render will show the fallback UI.
+      return { hasError: true };
+    }
+  
+    componentDidCatch(error: any, errorInfo: any) {
+      // You can also log the error to an error reporting service
+      console.error(error, errorInfo);
+    }
+  
+    render() {
+      if (this.state.hasError) {
+        // You can render any custom fallback UI
+        return <h1>Something went wrong.</h1>;
+      }
+  
+      return this.props.children; 
+    }
+  }
+
+  export default ErrorBoundary;
