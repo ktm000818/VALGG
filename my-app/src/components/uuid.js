0a695ed167e43b661a1887f1223c7ff0276f8841
taemin kim <97078686+ktm000818@users.noreply.github.com>
2023-03-11 01:44:05 +0900

    chore: delete js files

--- my-app/src/components/uuid.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeUUID = void 0;
-const makeUUID = () => {
-    return window.self.crypto.randomUUID();
-};
-exports.makeUUID = makeUUID;
